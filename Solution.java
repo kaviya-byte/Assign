d2fbc7dd18d55c1aed3dcaa22d6e1bedc9aef198
kaviya-byte <71174175+kaviya-byte@users.noreply.github.com>
2020-10-05 22:33:35 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+
+        while(in.hasNextInt())
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            myCalculator M = new myCalculator();
+            try
+            {
+                System.out.println(M.power(n,p));
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+
+
+	}
+
+}
