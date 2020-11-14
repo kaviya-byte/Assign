7016f658e154abde5eca16e9bc0328f8f127a5b9
kaviya-byte <71174175+kaviya-byte@users.noreply.github.com>
2020-11-14 22:51:32 +0530

    Add files via upload

---
+++ ngif-directive.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';  
+@Component({  
+   selector:'NgIfDirective',  
+   templateUrl:'ngif-directive.html'  
+})  
+export class NgIfComponent{  
+   typeOfEmployee:string="permanent";  
+} 
